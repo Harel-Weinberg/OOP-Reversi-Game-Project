f4eb863fc8b081fe25485a764ea828794e84220e
Harel Weinberg <harelweinberg@gmail.com>
2024-11-07 10:43:39 +0200

    GameLogic

--- GameLogic.java
+++ GameLogic.java
@@ -3,6 +3,11 @@ import java.util.List;
 
 public class GameLogic implements PlayableLogic {
 
+    public Disc [][] GameBoard = new Disc[8][8];
+    public List<Position> p1;
+    public List<Position> p2;
+
+
     @Override
     public boolean locate_disc(Position a, Disc disc) {
         if(getDiscAtPosition(a)!=null)return false;//בדיקה שהמיקום שהדיסק מעוניין להגיע איליו ריק
@@ -15,16 +20,24 @@ public class GameLogic implements PlayableLogic {
 
     @Override
     public Disc getDiscAtPosition(Position position) {
-        return null;
+        if(GameBoard[position.X][position.Y]==null)return null; // בודק אם המיקום שקיבלנו על הלוח הוא null ואם כן מחזיר null
+        return GameBoard[position.X][position.Y].getOwner(); // במידה והמיקום לא היה null הוא מחזיר למי שייך הדיסקית באותה נקודה}
     }
 
     @Override
     public int getBoardSize() {
-        return 0;
+        return 8;
     }
 
     @Override
     public List<Position> ValidMoves() {
+        Position(N) - Position(O) == A;
+        public boolean check( Position(N),A)
+        if(Position(O)-A == Position(X));
+            return true;
+        if (Position(O) == Position(O))
+            Position(O)==Position(O);
+            check(Position(O),A);
         return null;
     }
 
